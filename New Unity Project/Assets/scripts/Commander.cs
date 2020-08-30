f9598971320d00437e309b985c78458845943e0a
wormius51 <eyalhazor1@gmail.com>
2020-08-30 17:15:42 +0300

    worm commit

---
+++ New Unity Project/Assets/scripts/Commander.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commander : MonoBehaviour
+{
+    public static Commander instance { get; private set; }
+    public int[] letters = new int[26];
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
